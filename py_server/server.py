1421370a2562c9912ce88eea874b64096e21504a
jgalassi1 <jgalassi@nd.edu>
2022-11-02 17:39:50 -0400

    Added backend creatuser

--- py_server/server.py
+++ py_server/server.py
@@ -1,6 +1,7 @@
 from urllib.parse import _NetlocResultMixinBase
 from flask import Flask
 from flask_mysqldb import MySQL
+from flask import request
 import json
 
 app = Flask(__name__)
@@ -21,6 +22,24 @@ def podcasts():
     for result in rv:
         json_data.append(dict(zip(row_headers,result)))
     return json.dumps(json_data)
+
+@app.route('/createAccount', methods=['POST'])
+def parse_request():
+    data = request.json
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO users (UserID, username, password, firstname, lastname, phonenumber) VALUES ('{UserID}','{uname}','{pword}','{fname}','{lname}','{pnum}');".format(
+        UserID = 1,
+        uname = data['username'],
+        pword = data['password'],
+        fname = data['firstname'],
+        lname = data['lastname'],
+        pnum = data['phoneNumber']
+    ))
+    mysql.connection.commit()
+
+
+
+    return request.data
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
