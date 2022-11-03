a02315dd33b3c142c757a06c87d60c8ee78177d2
jflaher4 <jflaher4@nd.edu>
2022-11-02 22:23:03 -0400

    init plan trip page

--- client/pages/PlanTripPage.js
+++ client/pages/PlanTripPage.js
@@ -1,10 +1,41 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function PlanTripPage() {
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Plan Trip</Text>
+      <Text>Where are you departing from?</Text>
+      <TextInput
+        style={styles.TextInput}
+        placeholder="Address"
+        placeholderTextColor="#003f5c"
+        //onChangeText={}
+      />
+      <Text>What is your final destination?</Text>
+      <TextInput 
+        style={styles.TextInput}
+        placeholder="Address"
+        placeholderTextColor="#003f5c"
+        //onChangeText={}
+      />
+      <Text>Would you like to make any stops?</Text>
+
+      <Text>What sort of podcasts are you interested in listening to?</Text>
+
+      <TouchableOpacity>
+        <Text>Plan my trip!</Text>
+      </TouchableOpacity>
     </View>
   );
 }
